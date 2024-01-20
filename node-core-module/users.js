5a39e6c0018bd2873e46b810254432b0e80e1947
RatriaSeza <satriareza2002@gmail.com>
2024-01-20 19:11:53 +0700

    learn: user app finish

--- node-core-module/users.js
+++ node-core-module/users.js
@@ -24,16 +24,58 @@ const questions = (question) => {
 	});
 };
 
-const saveUser = (name, email, age) => {
-  const user = { name, email , age};
+const loadUser = () => {
 	const users = fs.readFileSync("./node-core-module/data/user.json", "utf8");
 	const data = JSON.parse(users);
+	return data;
+}
 
-	data.push(user);
-	fs.writeFileSync("./node-core-module/data/user.json", JSON.stringify(data, null, 2));
+const saveUser = (name, email, age) => {
+  const user = { name, email , age};
+	const users = loadUser();
+
+	const duplicateUser = users.find((user) => user.name.toLowerCase() === name.toLowerCase());
+
+	if (duplicateUser) {
+		return console.log("User already exists!");
+	}
+
+	users.push(user);
+	fs.writeFileSync("./node-core-module/data/user.json", JSON.stringify(users, null, 2));
 	console.log("Data has been written to file");
 
   readline.close()
 }
 
-module.exports = { questions, saveUser };
+const getAllUsers = () => {
+	const users = loadUser();
+	console.table(users);
+}
+
+const getUser = (name) => {
+	const users = loadUser();
+	const user = users.find((user) => user.name.toLowerCase() === name.toLowerCase());
+	
+	if (!user) {
+		return console.log("User not found!");
+	}
+
+	console.log("User detail:");
+	console.log(`Name: ${user.name}`);
+	console.log(`Email: ${user.email}`);
+	console.log(`Age: ${user.age}`);
+}
+
+const deleteUser = (name) => {
+	const users = loadUser();
+	const filteredUsers = users.filter((user) => user.name.toLowerCase() !== name.toLowerCase());
+
+	if (users.length === filteredUsers.length) {
+		return console.log("User not found!");
+	}
+
+	fs.writeFileSync("./node-core-module/data/user.json", JSON.stringify(filteredUsers, null, 2));
+	console.log(`User ${name} has been deleted!`);
+}
+
+module.exports = { questions, saveUser, getAllUsers, getUser, deleteUser };
--- node-core-module/yargs.js
+++ node-core-module/yargs.js
@@ -1,5 +1,5 @@
 const yargs = require('yargs');
-const { saveUser } = require('./users');
+const { saveUser, getAllUsers, getUser, deleteUser } = require('./users');
 
 yargs.command({
   command: 'add',
@@ -23,6 +23,45 @@ yargs.command({
   },
   handler(argv) {
     saveUser(argv.name, argv.email, argv.age)
+  }
+}).demandCommand()
+
+// show list of users
+yargs.command({
+  command: 'list',
+  describe: 'List all users',
+  handler() {
+    getAllUsers();
+  }
+})
+
+yargs.command({
+  command: 'detail',
+  describe: 'Show detail user',
+  builder: {
+    name: {
+      describe: 'User name',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler(argv) {
+    getUser(argv.name)
+  }
+})
+
+yargs.command({
+  command: 'delete',
+  describe: 'Delete user',
+  builder: {
+    name: {
+      describe: 'User name',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler(argv) {
+    deleteUser(argv.name)
   }
 })
 
